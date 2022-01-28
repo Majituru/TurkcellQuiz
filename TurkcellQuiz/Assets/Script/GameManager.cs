ed7a57b6118d4809722bc96e7abc22b71aa87d1e
Majituru <biltangurleme@gmail.com>
2022-01-28 15:38:31 +0300

    True&False Deneme

--- TurkcellQuiz/Assets/Script/GameManager.cs
+++ TurkcellQuiz/Assets/Script/GameManager.cs
@@ -1,18 +1,98 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using TMPro;
+using UnityEngine.SceneManagement;
+
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Question[] questions;
+    private static List<Question> unansweredQuestions;
+
+    private Question currentQuestion;
+
+    [SerializeField]
+    private TextMeshProUGUI factText;
+
+    [SerializeField]
+    private TextMeshProUGUI trueAnswerText;
+
+    [SerializeField]
+    private TextMeshProUGUI falseAnswerText;
+
+    [SerializeField]
+    private float timeBetweenQuestions = 1f;
+
+    [SerializeField]
+    private Animator animator;
+
+    private void Start()
     {
+        if (unansweredQuestions == null || unansweredQuestions.Count == 0)
+        {
+            unansweredQuestions = questions.ToList<Question>();
+        }
+
+        SetCurrentQuestion();
+    }
+
+    void SetCurrentQuestion() 
+    {
+        int randomQuestionIndex = Random.Range(0, unansweredQuestions.Count);
+        currentQuestion = unansweredQuestions[randomQuestionIndex];
+
+        factText.text =  currentQuestion.fact;
+
+        if(currentQuestion.isTrue)
+        {
+            trueAnswerText.text = "CORRECT";
+            falseAnswerText.text = "WRONG";
+        } else
+        {
+            trueAnswerText.text = "WRONG";
+            falseAnswerText.text = "CORRECT";
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator TransitionToNextQuestion()
     {
-        
+        unansweredQuestions.Remove(currentQuestion);
+
+        yield return new WaitForSeconds(timeBetweenQuestions);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void UserSellectTrue()
+    {
+        animator.SetTrigger("True");
+        if (currentQuestion.isTrue)
+        {
+            Debug.Log("Doğru");
+        } else
+        {
+            Debug.Log("Yanlış");
+        }
+
+        StartCoroutine(TransitionToNextQuestion());
+    }
+
+    public void UserSellectFalse()
+    {
+        animator.SetTrigger("False");
+        if (!currentQuestion.isTrue)
+        {
+            Debug.Log("Doğru");
+        }
+        else
+        {
+            Debug.Log("Yanlış");
+        }
+
+        StartCoroutine(TransitionToNextQuestion());
     }
 }
